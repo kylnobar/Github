d916f355791e034248e24523e4f2eb9dbf55b01f
kylnobar <43771969+kylnobar@users.noreply.github.com>
2019-02-20 17:16:01 -0800

    UPDATE 20022019

---
+++ assign02/assign02/assign02.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include "primeFactorization.h"
+
+int a = 0;
+
+int main() {
+
+	primeFactorization make1;
+	make1.factorization;
+	system("pause");
+	return 0;
+}
